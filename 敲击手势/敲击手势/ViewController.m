915e6a72a25535877817f30b5dd43b402b2c3955
AmberWhiteSky <amberwhitesky@163.com>
2015-10-01 23:39:08 +0800

    手势敲击

--- 敲击手势/敲击手势/ViewController.m
+++ 敲击手势/敲击手势/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIGestureRecognizerDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *iconView;
 
 @end
@@ -18,11 +18,49 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self  test2];
+   }
+
+
+
+-(void)  test2{
+    UITapGestureRecognizer *tap =[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapview)];
+    tap.delegate=self;
+    [self.iconView  addGestureRecognizer:tap];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)test{
+    //1.创建手势识别器对象
+    UITapGestureRecognizer *tap =[[UITapGestureRecognizer  alloc]init];
+    tap.numberOfTapsRequired =2;
+    tap.numberOfTouchesRequired=2;
+    
+    //2.将手势识别器对象添加到对应的view
+    [self .iconView addGestureRecognizer:tap];
+    
+    //3.添加肩痛方法（识别到了对应的手势，就会掉用监听方法）
+    [tap  addTarget:self action:@selector(tapview)];
+
+
 }
+
+#pragma  mark 代理方法
+-(BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch {
+
+    CGPoint  POS =[touch  locationInView:touch.view];
+    if (POS.x<=self.iconView.frame.size.width*0.5) {
+        return YES;
+        
+    }
+    
+    NSLog(@"%@",NSStringFromCGPoint(POS));
+    return NO;
+}
+
+
+-(void) tapview{
+    NSLog(@"tapview");
+}
+
 
 @end
